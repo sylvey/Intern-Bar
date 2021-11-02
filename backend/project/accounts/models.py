6176031c31d309588ad151270800b5cef2026e52
JC Kuan <guanjordan722@gmail.com>
2021-11-02 18:54:50 +0800

    updated relation schemas "User" and "Experience"

--- backend/project/accounts/models.py
+++ backend/project/accounts/models.py
@@ -2,28 +2,24 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=25)
+    user_id = models.CharField(max_length=25, primary_key=True)
+    user_name = models.CharField(max_length=25)
     password = models.CharField(max_length=40)    # set minimum password length
     status = models.BooleanField()
 
     def __unicode__(self):
         return self.username
 
-class Worked_As(models.Model):    # Relationship
-    # primary key?
-    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='UserWorksAs')
-    experience = models.ForeignKey('Experience', on_delete=models.CASCADE, related_name='WorksAsExp', null=True)
-
 class Experience(models.Model):
-    pos_id = models.AutoField(primary_key=True)
+    exp_id = models.AutoField(primary_key=True)
     pos_name = models.CharField(max_length=100)
-    org = models.ForeignKey('Organization', on_delete=models.CASCADE, related_name='PosInOrg')
+    org = models.ForeignKey('Organization', on_delete=models.CASCADE, related_name='ExpInOrg')
     start_time = models.DateField()
     end_time = models.DateField()
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='ExpOfUser')
 
     def __unicode__(self):
-        return u'%s %s' % (self.pos_name, self.org_name)
+        return u'%s %s %s' % (self.user, self.pos_name, self.org)
 
 class Organization(models.Model):
     org_id = models.AutoField(primary_key=True)
@@ -40,10 +36,10 @@ class Place(models.Model):
     district = models.CharField(max_length=50)
 
 class Located_In(models.Model):    # Relationship
-    # primary key?
+    # default primary key
     org = models.ForeignKey('Organization', on_delete=models.CASCADE, related_name='OrgLocatedIn')
     location = models.ForeignKey('Place', on_delete=models.CASCADE, related_name='LocatedInSite')
-    est_time = models.DateField()
+    est_time = models.DateField(null=True)
 
 class Post(models.Model):
     post_id = models.AutoField(primary_key=True)
@@ -72,7 +68,7 @@ class Category(models.Model):
         return u'%s %s' % (self.category_name, self.user)
 
 class Collected_In(models.Model):    # Relationship
-    # primary key?
+    # default primary key
     post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='PostCollectedIn')
     category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='CollectedInCategory')
 
