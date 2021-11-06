5d7aec3890b01938e884e909e074e9aeaf583fd6
minakim409 <44054303+minakim409@users.noreply.github.com>
2021-11-07 02:57:43 +0800

    post get all

--- backend/PostApp/views.py
+++ backend/PostApp/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import *
+from .serializer import *
 
 @api_view(['POST'])
 def post_create(request):
