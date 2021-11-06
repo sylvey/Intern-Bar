6243f043b6d43220a344006fdf89581119abd5fb
minakim409 <44054303+minakim409@users.noreply.github.com>
2021-11-06 23:02:35 +0800

    post create

--- backend/PostApp/urls.py
+++ backend/PostApp/urls.py
@@ -4,6 +4,5 @@ import PostApp.views as views
 
 urlpatterns = [
     ### Add urls in view.py here
-    # path('user/create_post', views.create_post),
-    path('post_getAll', views.post_getAll),
+    path('post/create', views.post_create),
 ]
