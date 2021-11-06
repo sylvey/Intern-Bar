ff50a23596784b47cfec6523fc935c36de153d5d
minakim409 <44054303+minakim409@users.noreply.github.com>
2021-11-07 02:52:19 +0800

    post_getAll

--- backend/PostApp/serializer.py
+++ backend/PostApp/serializer.py
@@ -6,3 +6,19 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
     model = Post
     fields = ('publisher', 'title', 'content', 'experience')
+
+     
+class ExperienceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Experience
+        exclude = ('user',)
+        depth = 1
+
+        
+class PostGetSerializer(serializers.ModelSerializer):
+    experience = ExperienceSerializer(read_only = True)
+
+    class Meta:
+        model = Post
+        fields = ('publisher_id', 'title', 'content', 'experience')
+        depth = 1
