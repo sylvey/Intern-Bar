9592eaa043ed71c4dace5eec1937b59e351c86f4
sofiak0426 <b07705007@ntu.edu.tw>
2021-11-07 15:28:58 +0800

    temporarily modify models.py position and experience

--- backend/ExpApp/models.py
+++ backend/ExpApp/models.py
@@ -1,15 +1,23 @@
 from django.db import models
+from django.db.models.fields import related
 
 class Experience(models.Model):
     exp_id = models.AutoField(primary_key=True)
-    pos_name = models.CharField(max_length=100)
     start_date = models.DateField()
     end_date = models.DateField()
-    org = models.ForeignKey('Organization', on_delete=models.CASCADE, related_name='ExpInOrg')
     user = models.ForeignKey('UserApp.User', on_delete=models.CASCADE, related_name='ExpOfUser')
 
     def __unicode__(self):
         return u'%s %s %s' % (self.user, self.pos_name, self.org)
+
+class Position(models.Model):
+    pos_id = models.AutoField(primary_key=True)
+    pos_name = models.CharField(max_length=100)
+    org = models.ForeignKey('Organization', on_delete=models.CASCADE, related_name='ExpInOrg')
+    place = models.ForeignKey('Place', on_delete=models.CASCADE, related_name='PlaceOfOrg')
+    salary = models.IntegerField(null=True, blank=True)
+    class Meta:
+        unique_together = (("pos_name", "org", "place"))
 
 class Organization(models.Model):
     org_id = models.AutoField(primary_key=True)
@@ -25,9 +33,9 @@ class Place(models.Model):
     city = models.CharField(max_length=50)
     district = models.CharField(max_length=50)
 
-class Located_In(models.Model):    # Relationship
-    org = models.ForeignKey('Organization',on_delete=models.CASCADE, related_name='OrgLocatedIn')
-    location = models.ForeignKey('Place', on_delete=models.CASCADE, related_name='LocatedInSite')
+# class Located_In(models.Model):    # Relationship
+#     org = models.ForeignKey('Organization',on_delete=models.CASCADE, related_name='OrgLocatedIn')
+#     location = models.ForeignKey('Place', on_delete=models.CASCADE, related_name='LocatedInSite')
 
-    class Meta:
-        unique_together = (("org", "location"))
+#     class Meta:
+#         unique_together = (("org", "location"))
