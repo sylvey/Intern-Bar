7ffae33b6a7ac141009e2f14658d3956a8506043
JC Kuan <guanjordan722@gmail.com>
2021-11-08 21:59:40 +0800

    updated comment apis

--- backend/CategoryApp/views.py
+++ backend/CategoryApp/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import *
+from .serializer import *
+
+@api_view(['POST'])
+def category_create(request):
+	if 'application/json' not in request.content_type:
+		return Response("Content type should be 'application/json'.", status=status.HTTP_400_BAD_REQUEST)
+
+	if request.method == 'POST':
+		user_id = request.data['user']
+		if check_login(user_id)['result'] == True:
+			serializer = CatSerializer(data=request.data)
+			if serializer.is_valid():
+				if serializer.create():
+					return Response(status=status.HTTP_201_CREATED)
+				else:
+					return Response("The name already exists.", status=status.HTTP_403_FORBIDDEN)
+			else:
+				return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_from_category(request):
+	if 'application/json' not in request.content_type:
+		return Response("Content type should be 'application/json'.", status=status.HTTP_400_BAD_REQUEST)
+
+	if request.method == 'GET':
+		user_id = request.data['user']
+		category = request.data['category_id']
+        if check_login(user_id)['result'] == True:
+            post_to_display = Post.objects.filter()    # joint: collected in
+
+
+
+
+
--- backend/PostApp/serializer.py
+++ backend/PostApp/serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import *
 from UserApp.serializer import UserSerializer
 from ExpApp.models import Experience
+from UserApp.models import User
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,3 +24,32 @@ class PostGetSerializer(serializers.ModelSerializer):
         model = Post
         fields = ('publisher_id', 'title', 'content', 'experience')
         depth = 1
+
+class CommentSerializer(serializers.ModelSerializer):
+    user_id = serializers.CharField()
+
+    class Meta:
+        model = Post
+        fields = {'comment_id', 'author', 'post_attached', 'content', 'published_time'}
+
+    def create(self):
+        new_comment = Comment(
+            author = User.objects.get(user_id = self.validated_data['author']),
+            post_attached = Post.objects.get(user_id = self.validated_data['post_attached']),
+            content = self.validated_data['content'],
+            published_time = self.validated_data['published_time']
+        )
+        new_comment.save()
+        return new_comment
+
+    def display(self, instance):
+        return {
+            "comment_id": instance.comment_id,
+            "author": instance.author,
+            "published_time": instance.published_time,
+            "content": instance.content
+        }
+
+
+
+
